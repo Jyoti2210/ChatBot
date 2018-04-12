b6a8d4b5c5e297fe6345133cdfac034544f58022
Jyoti2210 <bansaljyoti04@gmail.com>
2018-04-11 20:28:40 -0700

    modiying files

--- apis/time.js
+++ apis/time.js
@@ -1,40 +1,20 @@
 var r = require("../utils/runtime");
 
-function shuttleInfoHandler(req,res){
-	/*try{
-		req.body = JSON.parse(req.body);
-	} catch(e){
-		res.sendJson(500,{
-			code: 500,
-			message: "Failed to parse the input"
-		});
-		return;		
-	}*/
-
-	
-	
-		r.getRuntimeKey("db").mongo.users.find().toArray(function(err,item){
+function shuttleInfoHandler(req,res){	
+		r.getRuntimeKey("db").mongo.WaitingTime.find({name:"admin"}).toArray(function(err,item){
 			if(err || item.length == 0){
 				res.sendJson(400, {
 					code: 400,
 					message : "Shuttle time not available"
 				});
 			}else {
-				/*
-					item = {
-						'time': '20 mins'
-					}
-				*/
-				res.sendJson(200,{
-					code: 200,
-					data: {
-						'time': item[0].shuttleTime
-					}
-				});
+				var resp = {
+					speech: "Shuttle is available in " + item[0].waitTime,
+					displayText: "Shuttle is available in " + item[0].waitTime
+				};
+				res.sendJson(200, resp);
 			}
-		});
-	
-	
+		});	
 }
 
 module.exports = {
